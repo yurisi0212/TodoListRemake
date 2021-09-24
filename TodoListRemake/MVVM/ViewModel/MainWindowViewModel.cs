3cd4448d0b0c14210650b70641f65735e4e46c76
yurisi0212 <Dev.yurisi@gmail.com>
2021-09-25 01:56:32 +0900

    RightWindowCommandsの追加

--- TodoListRemake/MVVM/ViewModel/MainWindowViewModel.cs
+++ TodoListRemake/MVVM/ViewModel/MainWindowViewModel.cs
@@ -1,6 +1,18 @@
 ﻿using System.Windows;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
 
 namespace TodoListRemake.MVVM.ViewModel {
     public class MainWindowViewModel : DependencyObject {
+        public ICommand AddScheduleCommand { get; }
+        public ICommand ShowCalendarCommand { get; }
+
+        public MainWindowViewModel() {
+
+            AddScheduleCommand = new RelayCommand(() => {
+                MessageBox.Show("押せてる");
+            });
+
+        }
     }
 }

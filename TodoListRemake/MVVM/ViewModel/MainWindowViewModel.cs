c586931126f6366418ad120dc3e1f24072df6de9
yurisi0212 <Dev.yurisi@gmail.com>
2021-09-25 03:21:38 +0900

    TextのDependencyPropertyを登録

--- TodoListRemake/MVVM/ViewModel/MainWindowViewModel.cs
+++ TodoListRemake/MVVM/ViewModel/MainWindowViewModel.cs
@@ -4,6 +4,31 @@ using GalaSoft.MvvmLight.Command;
 
 namespace TodoListRemake.MVVM.ViewModel {
     public class MainWindowViewModel : DependencyObject {
+
+        public static readonly DependencyProperty TitleTextProperty = 
+            DependencyProperty.Register("TitleText", typeof(string), typeof(MainWindowViewModel), new UIPropertyMetadata("タイトル"));
+
+        public static readonly DependencyProperty ContentsTextProperty =
+            DependencyProperty.Register("ContentsText", typeof(string), typeof(MainWindowViewModel), new UIPropertyMetadata("コンテンツ"));
+
+        public static readonly DependencyProperty FooterTextProperty =
+            DependencyProperty.Register("FooterText", typeof(string), typeof(MainWindowViewModel), new UIPropertyMetadata("ようこそ"));
+
+        public string TitleText {
+            get => (string)GetValue(TitleTextProperty);
+            set => SetValue(TitleTextProperty, value);
+        }
+
+        public string ContentsText {
+            get => (string)GetValue(ContentsTextProperty);
+            set => SetValue(ContentsTextProperty, value);
+        }
+
+        public string FooterText {
+            get => (string)GetValue(FooterTextProperty);
+            set => SetValue(FooterTextProperty, value);
+        }
+
         public ICommand AddScheduleCommand { get; }
         public ICommand ShowCalendarCommand { get; }
 

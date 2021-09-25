e260c1fe5a8b1df7704bd6285713b98569ec746c
yurisi0212 <dev.yurisi@gmail.com>
2021-09-25 13:05:10 +0900

    カレンダーを実装

--- TodoListRemake/MVVM/ViewModel/MainWindowViewModel.cs
+++ TodoListRemake/MVVM/ViewModel/MainWindowViewModel.cs
@@ -1,9 +1,13 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Input;
 using GalaSoft.MvvmLight.Command;
 
 namespace TodoListRemake.MVVM.ViewModel {
     public class MainWindowViewModel : DependencyObject {
+
+        public static readonly DependencyProperty ShowCalendarIsOpenProperty =
+            DependencyProperty.Register("ShowCalendarIsOpen", typeof(bool), typeof(MainWindowViewModel), new UIPropertyMetadata(false));
 
         public static readonly DependencyProperty TitleTextProperty = 
             DependencyProperty.Register("TitleText", typeof(string), typeof(MainWindowViewModel), new UIPropertyMetadata("タイトル"));
@@ -13,6 +17,17 @@ namespace TodoListRemake.MVVM.ViewModel {
 
         public static readonly DependencyProperty FooterTextProperty =
             DependencyProperty.Register("FooterText", typeof(string), typeof(MainWindowViewModel), new UIPropertyMetadata("ようこそ"));
+
+        public static readonly DependencyProperty SelectedContentsDateTimeProperty =
+            DependencyProperty.Register("SelectedContentsDateTime", typeof(DateTime), typeof(MainWindowViewModel), new UIPropertyMetadata(DateTime.Now));
+
+        public static readonly DependencyProperty ViewDateProperty =
+            DependencyProperty.Register("ViewDate", typeof(DateTime), typeof(MainWindowViewModel), new UIPropertyMetadata(DateTime.Now));
+
+        public bool ShowCalendarIsOpen {
+            get => (bool)GetValue(ShowCalendarIsOpenProperty);
+            set => SetValue(ShowCalendarIsOpenProperty, value);
+        }
 
         public string TitleText {
             get => (string)GetValue(TitleTextProperty);
@@ -29,18 +44,38 @@ namespace TodoListRemake.MVVM.ViewModel {
             set => SetValue(FooterTextProperty, value);
         }
 
+        public DateTime SelectedContents {
+            get => (DateTime)GetValue(SelectedContentsDateTimeProperty);
+            set => SetValue(SelectedContentsDateTimeProperty, value);
+        }
+
+        public DateTime ViewDate {
+            get => (DateTime)GetValue(ViewDateProperty);
+            set => SetValue(ViewDateProperty, value);
+        }
+
         public ICommand AddScheduleCommand { get; }
         public ICommand ShowCalendarCommand { get; }
+        public ICommand ChangeDateCommand { get; }
 
         public MainWindowViewModel() {
-
             AddScheduleCommand = new RelayCommand(() => {
                 _ = MessageBox.Show("予定の追加");
             });
 
             ShowCalendarCommand = new RelayCommand(() => {
-                _ = MessageBox.Show("カレンダーの表示");
+                ShowCalendarIsOpen = true;
             });
+
+            ChangeDateCommand = new RelayCommand<string>(date => {
+                ShowCalendarIsOpen = false;
+                ChangeDate(DateTime.Parse(date));
+            });
+        }
+
+        private void ChangeDate(DateTime date) {
+            ViewDate = date;;
+            _ = MessageBox.Show(ViewDate.ToString());
         }
     }
 }
